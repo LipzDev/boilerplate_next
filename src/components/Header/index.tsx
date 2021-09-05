0209492cd44f91217ca1529ebff73db9d71807aa
Filipe Gomes <filipe.gt1998@outlook.com>
2021-09-04 22:59:24 -0300

    This commit fix eslint and prettier

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import * as Styles from './styles';
+import React from 'react'
+import * as Styles from './styles'
 
 const Header = () => {
   return (
     <Styles.Header>
       <h1>HEADER</h1>
     </Styles.Header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
